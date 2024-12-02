74977da28cdf89b5ba2439e3d01fe0a0804bd8a2
Luis Alonso <luisalonso97@gmail.com>
2024-12-02 14:05:51 -0600

    Added all day 1

--- 01_Historian_Hysteria/part2.py
+++ 01_Historian_Hysteria/part2.py
@@ -10,9 +10,17 @@ puzzle_data = fr.read_file("../puzzle_inputs/01.dat", parse_exp)
 l_col = sorted(list(map(lambda x: int(x[0]), puzzle_data)))
 r_col = sorted(list(map(lambda x: int(x[1]), puzzle_data)))
 
+def get_similarity_score(a, x):
+  if x in a: a[x] = a[x] + 1
+  else: a[x] = 1
+  return a
+
+r_dict = ft.reduce(get_similarity_score, r_col, dict())
+
 def ans_reduce(a, x):
-  return a + abs(l_col[x] - r_col[x])
+  if x in r_dict:
+    return a + (r_dict[x] * x)
+  return a
 
-pzzl_ans = ft.reduce(ans_reduce, range(len(l_col)), 0)
-
+pzzl_ans = ft.reduce(ans_reduce, l_col, 0)
 print(pzzl_ans)
